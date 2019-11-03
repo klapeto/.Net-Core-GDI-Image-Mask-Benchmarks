ac809d1c3d6b6b4e19a4cc6c7359ace2b4dbee76
Ioannis Panagiotopoulos <yiannispana@protonmail.com>
2019-11-03 18:09:35 +0200

    Added Results

--- src/ImageMaskBenchmarks/ImageMaskBenchmarks/Program.cs
+++ src/ImageMaskBenchmarks/ImageMaskBenchmarks/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace ImageMaskBenchmarks
 {
@@ -20,8 +24,8 @@ namespace ImageMaskBenchmarks
 			}
 			catch
 			{
-				width = 10000;
-				height = 10000;
+				width = 5000;
+				height = 5000;
 				Console.WriteLine($"Usage: ./ImageMaskBenchmarks width height");
 				Console.WriteLine($"Invalid arguments. Arguments will get default values (w: {width} ,h: {height})");
 			}
@@ -33,35 +37,52 @@ namespace ImageMaskBenchmarks
 			var timeElapsed = timeMeasurer.Measure(() => (bmp, mask) = ImageGenerator.CreateWithMask(width, height));
 
 			Console.WriteLine(
-				$"Image/Mask Generation (w: {width} ,h: {height}) took: {timeElapsed.TotalMilliseconds} ms");
+				$"Image/Mask Generation (w: {width},h: {height}) took: {timeElapsed.TotalMilliseconds} ms");
 
 			bmp.Save(originalFilename, ImageFormat.Png);
-			
-			
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_SetPixel), () => MaskApplier.ApplyMask_SetPixel(bmp, mask));
-			
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Marshal_Copy), () => MaskApplier.ApplyMask_Marshal_Copy(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Marshal_Copy_Span_MultiThread), () => MaskApplier.ApplyMask_Marshal_Copy_Span_MultiThread(bmp, mask));
 
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Span), () => MaskApplier.ApplyMask_Unsafe_Copy_Span(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Span), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Span(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Span), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Span(bmp, mask));
-
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy), () => MaskApplier.ApplyMask_Unsafe_Copy(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed(bmp, mask));
-			MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed_MultiThread), () => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed_MultiThread(bmp, mask));
+			var times = new List<TimeSpan>
+			{
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_SetPixel),
+					() => MaskApplier.ApplyMask_SetPixel(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Marshal_Copy),
+					() => MaskApplier.ApplyMask_Marshal_Copy(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Marshal_Copy_Span_MultiThread),
+					() => MaskApplier.ApplyMask_Marshal_Copy_Span_MultiThread(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Span),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Span(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Span),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Span(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Span),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Span(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy),
+					() => MaskApplier.ApplyMask_Unsafe_Copy(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Pipeline_Friendly),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Pipeline_Friendly(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked(bmp, mask)),
+				MeasureAction(timeMeasurer, nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed(bmp, mask)),
+				MeasureAction(timeMeasurer,
+					nameof(MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed_MultiThread),
+					() => MaskApplier.ApplyMask_Unsafe_Copy_Constant_Unrolled_Unchecked_Fixed_MultiThread(bmp, mask))
+			};
+			
+			File.AppendAllLines("times", new []{ string.Join(';', times.Select(span => span.TotalMilliseconds.ToString("F2"))), });
 		}
 
-		private static void MeasureAction(OperationTimeMeasurer timeMeasurer, string name, Func<Bitmap> action)
+		private static TimeSpan MeasureAction(OperationTimeMeasurer timeMeasurer, string name, Func<Bitmap> action)
 		{
 			Bitmap newBmp = null;
 			var timeElapsed = timeMeasurer.Measure(() => newBmp = action());
 			Console.WriteLine($"{name} took: {timeElapsed.TotalMilliseconds} ms");
 			newBmp.Save($@"{name}.png", ImageFormat.Png);
 			newBmp.Dispose();
+			return timeElapsed;
 		}
 	}
 }
